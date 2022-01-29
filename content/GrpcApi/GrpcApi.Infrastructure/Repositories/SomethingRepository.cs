2d747c6c1806da957f27e7cd34f0ae121c9eb093
Thiago F <tcfialho@gmail.com>
2022-01-29 02:50:15 -0300

    fix: grpc template (#6)

---
+++ content/GrpcApi/GrpcApi.Infrastructure/Repositories/SomethingRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+using Herald.EntityFramework.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+
+using GrpcApi.Application.Entities;
+using GrpcApi.Application.Infrastructure.Repositories;
+
+namespace GrpcApi.Infrastructure.Repositories
+{
+    public class SomethingRepository : Repository<Something>, ISomethingRepository
+    {
+        protected override IQueryable<Something> _query { get; set; }
+
+        public SomethingRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
